502a9ab72219fac39373e7537a8fdc3898e05378
Curtis St Pierre <cstpierre@aihs.ca>
2016-02-24 20:18:58 -0800

    changed id_index to media for readability changed value to media for readability

--- media_player.js
+++ media_player.js
@@ -24,25 +24,25 @@ function next(){
 //change the video to the clicked one
 function changeVideo(id){
     //make sure that the id is and integer
-    var id_index = parseInt(id);
+    var media = parseInt(id);
     // check if the source of the new media is the same as the current
-    if (iframe.src != 'https://riipen.mediacore.tv/media/id:'+ media_list[id_index][0] + '/embed_player'){
+    if (iframe.src != 'https://riipen.mediacore.tv/media/id:'+ media_list[media][0] + '/embed_player'){
         //check the index of the video, if it's the first make sure the previous button goes to the last video
-        if (id_index > 0){
-            iframe.dataset.previous = id_index - 1;
+        if (media > 0){
+            iframe.dataset.previous = media - 1;
         }
         else{
             iframe.dataset.previous = media_list.length - 1;
         }
         //check the index of the video, if it's the last make sure the next button goes to the first video
-        if (id_index < media_list.length-1){
-            iframe.dataset.next = id_index + 1;
+        if (media < media_list.length-1){
+            iframe.dataset.next = media + 1;
         }
         else{
             iframe.dataset.next = 0;
         }
         //update the source video to the new media
-        iframe.src = 'https://riipen.mediacore.tv/media/id:'+ media_list[id_index][0] + '/embed_player';
+        iframe.src = 'https://riipen.mediacore.tv/media/id:'+ media_list[media][0] + '/embed_player';
         var player = new playerjs.Player(iframe);
         //make sure the player is ready and start playing
         player.on('ready', function(){
@@ -53,8 +53,8 @@ function changeVideo(id){
             next();
         });
         //set the title and the description of the new media
-        title.html('<h4>' + media_list[id_index][1] + '</h4>');
-        description.html(media_list[id_index][2]);
+        title.html('<h4>' + media_list[media][1] + '</h4>');
+        description.html(media_list[media][2]);
     }
 }
 //call out to api to grab media and build a playlist
@@ -68,9 +68,9 @@ $.ajax({
        xhr.setRequestHeader ("Authorization", "Basic " + btoa('riipenchallenge@mediacore.com' + ":" + 'riipenchallenge'));
        },
        success: function(results) {
-            $.each(results.items, function(key, value){
+            $.each(results.items, function(key, media){
                 //stored the id, title, and description in a 2D array to easily access the data later
-                media_list.push([value.id, value.title, value.description_plain]);
+                media_list.push([media.id, media.title, media.description_plain]);
             });
        },
        complete: function(data) {
