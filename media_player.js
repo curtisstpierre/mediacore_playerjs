35568d0c6aece9b011be7bf2de10c0bc5a0b4135
Curtis St Pierre <cstpierre@aihs.ca>
2016-02-24 19:44:29 -0800

    Added functionality for auto-play, skip-back, skip-forward, play next on video end

---
+++ media_player.js
@@ -0,0 +1,91 @@
+var ids = [];
+var iframe, title, description, player; //initialize variables for global access
+
+// wait for document to be ready then initialize variables
+$(document).on('ready', function(){
+    iframe = $('iframe')[0];
+    title = $('#title');
+    description = $('#description');
+    player = new playerjs.Player(iframe);
+});
+
+//play the previous track by index
+function previous(){
+    var previous_id_location = iframe.dataset.previous;
+    changeVideo(previous_id_location);
+}
+
+//play the next track by index
+function next(){
+    var next_id_location = iframe.dataset.next;
+    changeVideo(next_id_location);
+}
+
+//change the video to the clicked one
+function changeVideo(id){
+    //make sure that the id is and integer
+    var id_index = parseInt(id);
+    // check if the source of the new media is the same as the current
+    if (iframe.src != 'https://riipen.mediacore.tv/media/id:'+ ids[id_index][0] + '/embed_player'){
+        //check the index of the video, if it's the first make sure the previous button goes to the last video
+        if (id_index > 0){
+            iframe.dataset.previous = id_index - 1;
+        }
+        else{
+            iframe.dataset.previous = ids.length - 1;
+        }
+        //check the index of the video, if it's the last make sure the next button goes to the first video
+        if (id_index < ids.length-1){
+            iframe.dataset.next = id_index + 1;
+        }
+        else{
+            iframe.dataset.next = 0;
+        }
+        //update the source video to the new media
+        iframe.src = 'https://riipen.mediacore.tv/media/id:'+ ids[id_index][0] + '/embed_player';
+        var player = new playerjs.Player(iframe);
+        //make sure the player is ready and start playing
+        player.on('ready', function(){
+            player.play();
+        });
+        //make sure when the video ends, it will start the next video
+        player.on('ended', function(){
+            next();
+        });
+        //set the title and the description of the new media
+        title.html('<h4>' + ids[id_index][1] + '</h4>');
+        description.html(ids[id_index][2]);
+    }
+}
+//call out to api to grab media and build a playlist
+$.ajax({
+       type: 'GET',
+       dataType: "json",
+       url: 'https://riipen.mediacore.tv/api2/media',
+       data: {},
+       crossDomain: true,
+       beforeSend: function (xhr) {
+       xhr.setRequestHeader ("Authorization", "Basic " + btoa('riipenchallenge@mediacore.com' + ":" + 'riipenchallenge'));
+       },
+       success: function(results) {
+            $.each(results.items, function(key, value){
+                //stored the id, title, and description in a 2D array to easily access the data later
+                ids.push([value.id, value.title, value.description_plain]);
+                //            $('#tracks').append(['<li class="track" onclick="changeVideo('+ key +')">',
+                //                                '<div class="panel">',
+                //                                '<div class="row">',
+                //                                '<div class="large-9 medium-9 small-9 columns">',
+                //                                '<h4>'+value.title+'</h4>',
+                //                                '<p>'+value.description_plain+'</p>',
+                //                                '</div>',
+                //                                '</div>',
+                //                                '<div class="iframe" >' + 'https://riipen.mediacore.tv/media/id:'+value.id + '/embed_player' + '</div>',
+                //                                '</div>',
+                //                                '</li>'].join(' '));
+            });
+       },
+       complete: function(data) {
+            //when the data is retrieved from the request, start the first video
+            changeVideo(0);
+       }
+});
